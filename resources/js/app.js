a6d6ffbe7a1ef34f225d796fb2ae7699553d878d
Joan Vasquez <yoloprogramo22@gmail.com>
2019-03-31 21:21:53 -0400

    map added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 (function() {
   $(document).ready(function() {
     $("#menu-toggle").click(function(e) {
@@ -6,7 +5,85 @@
       $("#wrapper").toggleClass("toggled");
     });
 
-    Chart.defaults.global.responsive = true;
+    var mapData = {
+      US: 298,
+      SA: 200,
+      AU: 760,
+      IN: 2000000,
+      GB: 120
+    };
+
+    $("#world-map-markers").vectorMap({
+      map: "world_mill_en",
+      borderColor: "#fff",
+      borderOpacity: 0.25,
+      borderWidth: 0,
+      color: "#e6e6e6",
+      regionStyle: {
+        initial: {
+          fill: "#f4f4f4"
+        }
+      },
+
+      markerStyle: {
+        initial: {
+          r: 5,
+          fill: "#fff",
+          "fill-opacity": 1,
+          stroke: "#000",
+          "stroke-width": 1,
+          "stroke-opacity": 0.4
+        }
+      },
+
+      markers: [
+        {
+          latLng: [21.0, 78.0],
+          name: "INDIA : 350"
+        },
+        {
+          latLng: [-33.0, 151.0],
+          name: "Australia : 250"
+        },
+        {
+          latLng: [36.77, -119.41],
+          name: "USA : 250"
+        },
+        {
+          latLng: [55.37, -3.41],
+          name: "UK   : 250"
+        },
+        {
+          latLng: [25.2, 55.27],
+          name: "UAE : 250"
+        }
+      ],
+
+      series: {
+        regions: [
+          {
+            values: {
+              US: "#49c5b6",
+              SA: "#667add",
+              AU: "#50d38a",
+              IN: "#60bafd",
+              GB: "#ff758e"
+            },
+            attribute: "fill"
+          }
+        ]
+      },
+      hoverOpacity: null,
+      normalizeFunction: "linear",
+      zoomOnScroll: false,
+      scaleColors: ["#000000", "#000000"],
+      selectedColor: "#000000",
+      selectedRegions: [],
+      enableZoom: false,
+      hoverColor: "#fff"
+    });
+
+    /*Chart.defaults.global.responsive = true;
     Chart.defaults.global.maintainAspectRatio = false;
 
     let linearChart = document.getElementById('pie-chart').getContext('2d');
@@ -37,9 +114,9 @@
           }
         ]
       }
-    });
+    });*/
 
-   /* let pieChart = document.getElementById('pie-chart').getContext('2d');
+    /* let pieChart = document.getElementById('pie-chart').getContext('2d');
 
     new Chart(pieChart, {
       type: 'polarArea',
@@ -69,6 +146,5 @@
         ]
       }
     });*/
-
   });
 })();
