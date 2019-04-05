08791e89f514e5e07775ca4dbe86b4010c0ad056
Joan Vasquez <yoloprogramo22@gmail.com>
2019-04-04 17:00:31 -0800

    dashboard done

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
       $("#wrapper").toggleClass("toggled");
     });
 
-    if($("#world-map-markers").length) {
+    if ($("#world-map-markers").length) {
       $("#world-map-markers").vectorMap({
         map: "world_mill_en",
         borderColor: "#fff",
@@ -17,7 +17,7 @@
             fill: "#f4f4f4"
           }
         },
-  
+
         markerStyle: {
           initial: {
             r: 5,
@@ -28,7 +28,7 @@
             "stroke-opacity": 0.4
           }
         },
-  
+
         markers: [
           {
             latLng: [21.0, 78.0],
@@ -51,7 +51,7 @@
             name: "UAE : 250"
           }
         ],
-  
+
         series: {
           regions: [
             {
@@ -79,29 +79,21 @@
 
     Chart.defaults.global.responsive = true;
 
-    let linearChart = document.getElementById('linear-chart');
+    let linearChart = document.getElementById("linear-chart");
 
-    if(linearChart) {
-      linearChart = linearChart.getContext('2d');
+    if (linearChart) {
+      linearChart = linearChart.getContext("2d");
       new Chart(linearChart, {
-        type: 'line',
+        type: "line",
         data: {
-          labels: ['JavaScript', 'PHP', '.NET', 'Java', 'R'],
+          labels: ["JavaScript", "PHP", ".NET", "Java", "R"],
           datasets: [
             {
-              label: 'POPULARITY',
-              data: [
-                617594,
-                181045,
-                153060,
-                706953,
-                105162
-              ],
-              backgroundColor: [
-                '#0984e3'
-              ],
+              label: "POPULARITY",
+              data: [617594, 181045, 153060, 706953, 105162],
+              backgroundColor: ["#0984e3"],
               borderWidth: 2,
-              borderColor: '#777'
+              borderColor: "#777"
               //backgroundColor: 'green'
             }
           ]
@@ -109,26 +101,18 @@
       });
     }
 
-    let pieChart = document.getElementById('pie-chart');
+    let pieChart = document.getElementById("pie-chart");
 
-    if(document.getElementById('pie-chart')) {
-      pieChart = pieChart.getContext('2d');
+    if (document.getElementById("pie-chart")) {
+      pieChart = pieChart.getContext("2d");
       new Chart(pieChart, {
-        type: 'doughnut',
+        type: "doughnut",
         data: {
-          labels: ['Websites', 'Mobile Apps', 'Webservice'],
+          labels: ["Websites", "Mobile Apps", "Webservice"],
           datasets: [
             {
-              data: [
-                600,
-                300,
-                150,
-              ],
-              backgroundColor: [
-                '#3498db',
-                '#e74c3c',
-                '#2ecc71'
-              ],
+              data: [600, 300, 150],
+              backgroundColor: ["#3498db", "#e74c3c", "#2ecc71"],
               borderWidth: 1
             }
           ]
@@ -136,25 +120,23 @@
       });
     }
 
-    let tasksChart = document.getElementById('tasks-chart');
+    let tasksChart = document.getElementById("tasks-chart");
 
-    if(document.getElementById('tasks-chart')) {
-      tasksChart = tasksChart.getContext('2d');
+    if (document.getElementById("tasks-chart")) {
+      tasksChart = tasksChart.getContext("2d");
       new Chart(tasksChart, {
-        type: 'bar',
+        type: "bar",
         data: {
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+          labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
           datasets: [
             {
-              data: [
-                505, 409, 654, 800, 723
-              ],
+              data: [505, 409, 654, 800, 723],
               backgroundColor: [
-                '#27ae60',
-                '#27ae60',
-                '#27ae60',
-                '#27ae60',
-                '#27ae60'
+                "#27ae60",
+                "#27ae60",
+                "#27ae60",
+                "#27ae60",
+                "#27ae60"
               ]
               //backgroundColor: 'green'
             }
@@ -163,25 +145,19 @@
       });
     }
 
-    let productionChart = document.getElementById('production-chart');
+    let productionChart = document.getElementById("production-chart");
 
-    if(document.getElementById('production-chart')) {
-      productionChart = productionChart.getContext('2d');
+    if (document.getElementById("production-chart")) {
+      productionChart = productionChart.getContext("2d");
       new Chart(productionChart, {
-        type: 'polarArea',
+        type: "polarArea",
         data: {
-          labels: ['Blog', 'E-Commerce', 'CMS'],
+          labels: ["Blog", "E-Commerce", "CMS"],
           datasets: [
             {
-              label: 'HOMEWORKS HISTORY',
-              data: [
-                60, 30, 80
-              ],
-              backgroundColor: [
-                '#e74c3c',
-                '#2980b9',
-                '#8e44ad'
-              ]
+              label: "HOMEWORKS HISTORY",
+              data: [60, 30, 80],
+              backgroundColor: ["#e74c3c", "#2980b9", "#8e44ad"]
               //backgroundColor: 'green'
             }
           ]
@@ -189,17 +165,27 @@
       });
     }
 
-    let notifications = document.querySelectorAll('.btn-notification');
+    let notifications = document.querySelectorAll(".btn-notification");
 
-    if(notifications.length) {
+    if (notifications.length) {
+      let positions = null;
+      toastr.options.closeButton = true;
+
       notifications.forEach((item, index) => {
-        item.addEventListener('click', function(event) {
-          if(index === 0) {
-            toastr.success('Notification Top Right')
+        item.addEventListener("click", () => {
+          positions = $("#positions").val();
+          toastr.options.positionClass = positions;
+          if (index === 0) {
+            toastr.success("Success Notification");
+          } else if(index === 1) {
+            toastr.error("Error Notification");
+          } else if(index === 2) {
+            toastr.warning("Warning Notification");
+          } else if(index === 3) {
+            toastr.info("Info Notification");
           }
         });
       });
     }
-
   });
 })();
